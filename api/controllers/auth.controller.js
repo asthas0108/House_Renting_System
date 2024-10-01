b39628917b897ce8ac9ad0f6d0a771b32f4cb71f
asthas0108 <asthas0108@gmail.com>
2024-10-01 22:26:59 +0530

    create signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const signin = async (req, res, next) => {
         if(!validUser) return next(errorHandler(404), "user not found");
 
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(404), "wrong credentials");
+        if(!validPassword) return next(errorHandler(401), "wrong credentials");
 
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
