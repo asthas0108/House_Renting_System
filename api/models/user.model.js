d55e6d0be9666335d9aade7cdab098ce318d21a9
asthas0108 <asthas0108@gmail.com>
2024-10-10 15:21:34 +0530

    update header and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhwaLDKaK49tsHmdMGOrmTdns5qiw080F2Yw&s"
+        default: "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
     },
 }, {timestamps: true});
 
